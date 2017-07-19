a3c88593f5728ff18a3641db229bf6d0fa752ce4
Daniel <mail@danielluedecke.de>
2017-07-19 21:04:12 +0200

    update startup-msg

--- R/zzz.R
+++ R/zzz.R
@@ -1,6 +1,6 @@
 .onAttach <- function(libname, pkgname) {
   if (stats::runif(1) > .8) {
-    packageStartupMessage("Visit http://strengejacke.de/sjPlot for package-vignettes.")
+    packageStartupMessage("Learn more about sjPlot with 'browseVignettes(\"sjPlot\")'.")
   } else if (stats::runif(1) > .9) {
     packageStartupMessage("#refugeeswelcome")
   }
