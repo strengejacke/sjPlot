5b6d078d8ecd3e33738fe0801da5b42b829b5fca
Daniel <mail@danielluedecke.de>
2017-08-04 20:59:27 +0200

    change pkg-startup-msg

--- R/zzz.R
+++ R/zzz.R
@@ -1,7 +1,9 @@
 .onAttach <- function(libname, pkgname) {
   if (stats::runif(1) > .8) {
     packageStartupMessage("Learn more about sjPlot with 'browseVignettes(\"sjPlot\")'.")
-  } else if (stats::runif(1) > .9) {
+  } else if (stats::runif(1) > .8) {
+    packageStartupMessage("Install package \"strengejacke\" from GitHub (`devtools::install_github(\"strengejacke/strengejacke\")`) to load all sj-packages at once!")
+  } else if (stats::runif(1) > .8) {
     packageStartupMessage("#refugeeswelcome")
   }
 }
