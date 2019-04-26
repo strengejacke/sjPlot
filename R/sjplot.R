c7c7376c065a1a06b8df2213e314032ebd16333b
Daniel Lüdecke <mail@danielluedecke.de>
2019-04-26 17:25:14 +0200

    remove outdated examples

--- R/sjplot.R
+++ R/sjplot.R
@@ -49,27 +49,12 @@
 #' library(dplyr)
 #' data(efc)
 #'
-#' # Frequency plot
-#' sjplot(efc, e42dep, c172code, fun = "frq")
-#'
 #' # Grouped frequencies
 #' efc %>% sjplot(e42dep, c172code, fun = "grpfrq")
 #'
 #' # Grouped frequencies, as box plots
 #' efc %>% sjplot(e17age, c172code, fun = "grpfrq",
 #'                type = "box", geom.colors = "Set1")
-#'
-#' # frequencies, as plot grid
-#' efc %>%
-#'   select(e42dep, c172code, e16sex, c161sex) %>%
-#'   sjplot() %>%
-#'   plot_grid()
-#'
-#' # plot grouped data frame
-#' efc %>%
-#'   group_by(e16sex, c172code) %>%
-#'   select(e42dep, e16sex, c172code) %>%
-#'   sjplot(wrap.title = 100) # no line break for subtitles
 #'
 #' \dontrun{
 #' # table output of grouped data frame
