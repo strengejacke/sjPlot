b94917e8978f7898ed8681860cc8c42434b12abf
Daniel Lüdecke <mail@danielluedecke.de>
2019-09-25 15:40:20 +0200

    #close 550

--- R/plot_stackfrq.R
+++ R/plot_stackfrq.R
@@ -225,7 +225,8 @@ plot_stackfrq <- function(items,
   # passed as parameter
 
   if (is.null(weight.by)) {
-    dummy <- sjmisc::frq(items, show.na = FALSE)
+    dummy <- sjmisc::frq(items, show.na = TRUE)
+    dummy <- lapply(dummy, function(.i) .i[-nrow(.i), ])
   } else {
     items$weights <- weight.by
     dummy <- sjmisc::frq(items, weights = items$weights)
