84f9138365484ef17b61beead84dd5fca8f0e6f0
Daniel <mail@danielluedecke.de>
2025-07-01 17:57:49 +0200

    remove startup msg

--- R/zzz.R
+++
@@ -1,9 +0,0 @@
-.onAttach <- function(libname, pkgname) {
-  if (stats::runif(1) > .8) {
-    packageStartupMessage("Learn more about sjPlot with 'browseVignettes(\"sjPlot\")'.")
-  } else if (stats::runif(1) > .8) {
-    packageStartupMessage("Install package \"strengejacke\" from GitHub (`devtools::install_github(\"strengejacke/strengejacke\")`) to load all sj-packages at once!")
-  } else if (stats::runif(1) > .8) {
-    packageStartupMessage("#refugeeswelcome")
-  }
-}
